50b46581656debfa1987695664e3b82e74a60490
Raymond <raymondjohnson@pursuit.org>
2024-07-22 14:18:00 -0400

    style: image arrangement fix

--- src/components/AdContainer.jsx
+++ src/components/AdContainer.jsx
@@ -6,26 +6,40 @@ import blackPants04 from "../assets/black-pants04.png"
 const AdContainer = () => {
   return (
     <div className="AdContainer">
-      <h3>This is our AdContanier Component</h3>
+      <h4 className="adContainer__header">This is our AdContanier Component</h4>
       <div className="image-container">
-        <figure>
-          <img className="adContainter__img" src={blackPants01} alt="Libins Men's Dress..."/>
+        <div className="figure">
+          <img className="adContainer__img" src={blackPants01} alt="Libins Men's Dress..."/>
+          <p>Libins Men's Dress...</p>
+        </div>
+        <div className="figure">
+          <img className="adContainer__img" src={blackPants02} alt="Van Heusen Men's"/>
+          <p>Van Heusen Men's</p>
+        </div>
+        <div className="figure">
+          <img className="adContainer__img" src={blackPants03} alt="Dress pants closout..."/>
+          <p>Dress pants closout...</p>
+        </div>
+        <div className="figure">
+          <img className="adContainer__img" src={blackPants04} alt="Wholesale Men's Dress Pants"/>
+          <p>Wholesale Men's Dress Pants</p>
+        </div>
+        {/* <figure>
+          <img className="adContainer__img" src={blackPants01} alt="Libins Men's Dress..."/>
           <figcaption><p>Libins Men's Dress...</p></figcaption>
-        </figure>
-        <figure>
-          <img className="adContainter__img" src={blackPants02} alt="Van Heusen Men's"/>
+        </figure> */}
+        {/* <figure>
+          <img className="adContainer__img" src={blackPants02} alt="Van Heusen Men's"/>
           <figcaption><p>Van Heusen Men's</p></figcaption>
         </figure>
         <figure>
-          <img className="adContainter__img" src={blackPants03} alt="Libins Men's Dress..."/>
+          <img className="adContainer__img" src={blackPants03} alt="Libins Men's Dress..."/>
           <figcaption><p>Dress pants closout...</p></figcaption>
         </figure>
         <figure>
-          <img className="adContainter__img" src={blackPants04} alt="Van Heusen Men's"/>
+          <img className="adContainer__img" src={blackPants04} alt="Wholesale Men's Dress Pants"/>
           <figcaption><p>Wholesale Men's Dress Pants</p></figcaption>
-        </figure>
-      
-        
+        </figure> */}
       </div>
 
     </div>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,14 +2,14 @@ import './Nav.css'
 const Nav = () => {
   return (
    
-      <nav>
-        <span className='logo'>Logo</span>
-        <ul className='menu'>
-          <li className='menu__item'>Component1</li>
-          <li className='menu__item'>Display</li>
-           {/* BEM  BLOCK ELEMENT Modifier */}
-        </ul>
-      </nav>
+    <nav>
+      <span className='logo'>Logo</span>
+      <ul className='menu'>
+        <li className='menu__item'>Component1</li>
+        <li className='menu__item'>Display</li>
+          {/* BEM  BLOCK ELEMENT Modifier */}
+      </ul>
+    </nav>
     
   );
 }

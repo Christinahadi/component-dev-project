d06fc4031769a52c8638d0e9469fbb57ad4f133e
Raymond <raymondjohnson@pursuit.org>
2024-07-22 12:03:56 -0400

    style: updated css to fit images within containers

--- src/components/AdContainer.jsx
+++ src/components/AdContainer.jsx
@@ -1,6 +1,8 @@
 import "./AdContainer.css"
 import blackPants01 from "../assets/black-pants01.png"
 import blackPants02 from "../assets/black-pants02.png"
+import blackPants03 from "../assets/black-pants03.png"
+import blackPants04 from "../assets/black-pants04.png"
 const AdContainer = () => {
   return (
     <div className="AdContainer">
@@ -15,21 +17,14 @@ const AdContainer = () => {
           <figcaption><p>Van Heusen Men's</p></figcaption>
         </figure>
         <figure>
-          <img className="adContainter__img" src={blackPants01} alt="Libins Men's Dress..."/>
-          <figcaption><p>Libins Men's Dress...</p></figcaption>
+          <img className="adContainter__img" src={blackPants03} alt="Libins Men's Dress..."/>
+          <figcaption><p>Dress pants closout...</p></figcaption>
         </figure>
         <figure>
-          <img className="adContainter__img" src={blackPants02} alt="Van Heusen Men's"/>
-          <figcaption><p>Van Heusen Men's</p></figcaption>
+          <img className="adContainter__img" src={blackPants04} alt="Van Heusen Men's"/>
+          <figcaption><p>Wholesale Men's Dress Pants</p></figcaption>
         </figure>
-        <figure>
-          <img className="adContainter__img" src={blackPants01} alt="Libins Men's Dress..."/>
-          <figcaption><p>Libins Men's Dress...</p></figcaption>
-        </figure>
-        <figure>
-          <img className="adContainter__img" src={blackPants02} alt="Van Heusen Men's"/>
-          <figcaption><p>Van Heusen Men's</p></figcaption>
-        </figure>
+      
         
       </div>
 
